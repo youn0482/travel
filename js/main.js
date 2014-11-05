b0733ad1e36347a15f1f45f745c67c7134844373
m4ttyng <youn0482@algonquinlive.com>
2014-11-05 11:23:24 -0500

    slideshow bugs

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", init);
 
 function init(){
 	
-	$("#slideshow > div:gt(0)").fadeOut(1000);
+	
 
 setInterval(function() { 
   $('#slideshow > div:first')
