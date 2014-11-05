2af00b4e48cc851c20ea7115cef661668bde6c99
m4ttyng <youn0482@algonquinlive.com>
2014-11-05 11:07:50 -0500

    added slideshow

--- js/main.js
+++ js/main.js
@@ -2,6 +2,16 @@
 document.addEventListener("DOMContentLoaded", init);
 
 function init(){
-	//startup script
 	
-}
+	$("#slideshow > div:gt(0)").fadeOut(1000);
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(5000)
+    .next()
+    .fadeIn(2000)
+    .end()
+    .appendTo('#slideshow');
+},  7000);
+ 
+};
